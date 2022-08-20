3b41a25b84de1885721fa3616a7203ff1345253a
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-08-20 01:45:22 -0300

    Unsubscribe

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,6 +1,7 @@
+import { Subscription } from 'rxjs';
 import { AcoesService } from './acoes.service';
 import { Acoes } from './modelo/acoes';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -8,15 +9,21 @@ import { FormControl } from '@angular/forms';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit {
+export class AcoesComponent implements OnInit, OnDestroy {
   acoesInput = new FormControl();
   acoes: Acoes;
+  private subscription: Subscription;
 
   constructor(private acoesService: AcoesService) {}
 
   ngOnInit(): void {
-    this.acoesService.getAcoes().subscribe((acoes) => {
+    this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
       this.acoes = acoes;
     })
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, tap, pluck } from 'rxjs/operators';
-import { Acao } from './modelo/acoes';
+import { Acao, AcoesAPI } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class AcoesService {
 
   getAcoes() {
     return this.httpClient
-    .get<any>('http://localhost:3000/acoes')
+    .get<AcoesAPI>('http://localhost:3000/acoes')
     .pipe(
       tap((valor) => console.log(valor)),
       pluck('payload'),
