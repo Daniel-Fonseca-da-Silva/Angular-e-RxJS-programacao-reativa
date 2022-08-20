f55ee8a6b7bdf817bb2ff0461a8c314360f2b805
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-08-20 02:33:36 -0300

    Async pipe

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,6 +1,5 @@
-import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
-import { Acoes } from './modelo/acoes';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
@@ -11,7 +10,7 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesService.getAcoes();
+  acoes$ = this.acoesInput.valueChanges.pipe(tap(console.log));
 
   constructor(private acoesService: AcoesService) {}
 
