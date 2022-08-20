1fb4ddf588eb9b5aef9ee27c998f5876d24790f8
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-08-19 23:02:06 -0300

    Operador Map

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,21 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) { }
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes')
+    return this.httpClient
+    .get<any>('http://localhost:3000/acoes')
+    .pipe(map((acoes) => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))));
   }
 
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if(acaoA.codigo > acaoB.codigo)
+      return 1;
+    if(acaoA.codigo < acaoB.codigo)
+      return -1;
+
+    return 0;
+  }
+
+
 }
+
+
